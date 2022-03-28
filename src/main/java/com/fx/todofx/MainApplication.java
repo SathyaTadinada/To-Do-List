aa2c0ffdbb6a82ff19f890a940f85f02f3a3272a
Sathya Tadinada <57514020+SathyaTadinada@users.noreply.github.com>
2022-03-28 08:07:56 -0600

    added different locations for image icons

--- src/main/java/com/fx/todofx/MainApplication.java
+++ src/main/java/com/fx/todofx/MainApplication.java
@@ -15,11 +15,13 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        String fileLocation = "C:\\Users\\sathy\\Downloads\\to do list icon.jpg";
+        String fileLocationCSD = "C:\\Users\\stad5246\\Downloads\\to do list logo.png";
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("todo.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("To Do List");
         stage.setScene(scene);
-        stage.getIcons().add(new Image("C:\\Users\\sathy\\Downloads\\to do list icon.jpg"));
+        stage.getIcons().add(new Image(fileLocationCSD)); // make sure to change depending on where I am
         stage.setMaximized(true);
         stage.show();
     }
